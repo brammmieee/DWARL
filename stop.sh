09bb932763479c89c296683762a8da460f5f91a5
Bram Odrosslij <bram.odrosslij@nobleo.nl>
2024-01-08 14:30:51 +0100

    Bugfix in bash scripts

--- stop.sh
+++ stop.sh
@@ -1,5 +1,6 @@
 # Disable connections to the X server
 xhost -local:root > /dev/null 2>&1
 
-# Stop the container (check if container persists with `docker ps`)
+# Stop and remove`` the container and freeing up memory (check if container persists with `docker ps`)
 docker stop my_dwarl_container
+docker rm my_dwarl_container
