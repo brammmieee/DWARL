bcce087a1fd434b9c31bda69e4e7975ce35d0b56
Bram Odrosslij <bram.odrosslij@nobleo.nl>
2024-05-17 11:12:22 +0200

    Added the option to locally install the package in a python venv

---
+++ scripts/setup_locally.bash
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Check if the current system is Ubuntu 22.04 Desktop
+if [[ $(lsb_release -rs) == "22.04" && $(lsb_release -is) == "Ubuntu" && $(uname -m) == "x86_64" ]]; then
+    echo "Detected Ubuntu 22.04 Desktop. Continuing..."
+else
+    echo "Python venv install of package only compatible with Ubuntu 22.04. Consider using the dockerized version."
+    exit 1
+fi
+
+# Installing Webots 2023b
+echo "Installing Webots 2023b..."
+wget -qO- https://cyberbotics.com/Cyberbotics.asc | sudo apt-key add -
+sudo add-apt-repository 'deb https://cyberbotics.com/debian/ binary-amd64/'
+sudo apt update -y
+sudo apt install -y webots
+
+# Perform system update and upgrade
+sudo apt update -y
+sudo apt upgrade -y
+
+# Install Python 3 pip
+sudo apt install -y python3-pip
+
+# Get the parent directory path
+parent_dir=$(dirname "$(pwd)")
+
+# Create Python virtual environment in parent directory
+echo "Creating Python virtual environment in $parent_dir..."
+python3 -m venv "$parent_dir/venv"
+
+# Environment variable setup to be appended in the virtual environment activation script
+echo "
+# Custom environment variables for Webots and other settings
+export WEBOTS_HOME='/usr/local/webots'
+export PYTHONPATH='/usr/local/webots/lib/controller/python:$parent_dir:$parent_dir/venv/lib/python3.10/site-packages'
+export PYTHONIOENCODING='UTF-8'
+export TF_ENABLE_ONEDNN_OPTS=0
+" >> "$parent_dir/venv/bin/activate"
+
+# Activate virtual environment
+source "$parent_dir/venv/bin/activate"
+
+# Ensure the venv's bin directory is in the PATH
+export PATH="$parent_dir/venv/bin:$PATH"
+
+# Install PyTorch specifically for CUDA 11.8
+pip install torch --index-url https://download.pytorch.org/whl/cu118
+
+# Install other Python packages from requirements.txt
+echo "Installing Python packages..."
+pip install -r "$parent_dir/requirements.txt"
+
+# Deactivate virtual environment
+deactivate
+
+echo "Python packages installed successfully."
+echo "Webots 2023b and environment setup completed."
