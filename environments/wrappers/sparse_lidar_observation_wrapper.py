73c0d2bb81664fa3b34e74702565910058682ff3
Bram Odrosslij <bram.odros@gmail.com>
2024-08-27 18:03:03 +0200

    Updated the live plotting

--- environments/wrappers/sparse_lidar_observation_wrapper.py
+++ environments/wrappers/sparse_lidar_observation_wrapper.py
@@ -101,6 +101,9 @@ class SparseLidarObservationWrapper(gym.ObservationWrapper):
         if self.params['goal_pos_dist_max'] != self.params['proto_substitutions']['maxRange'] \
             or self.params['goal_pos_dist_min'] != self.params['proto_substitutions']['minRange']:
             print("Warning: The max/min goal distance is not equal to the max/min range of the lidar sensor. This may decrease consistency in the observation space.")
+        
+        if self.unwrapped.plot_wrapped_state == True:
+            self.init_plot()
 
     def observation(self, obs):
         # Lidar range normalization and invalid value removal
@@ -149,18 +152,23 @@ class SparseLidarObservationWrapper(gym.ObservationWrapper):
 
         return self.obs
     
-    def plot_observation(self, lidar_range_image, goal_angle, goal_distance):
-        try:
-            self.fig9 is None
-        except Exception:
-            self.fig9, self.ax9 = plt.subplots()
-            self.ax9.set_xlim(0, 1)  # Set the x-axis limits to 0 and 1
-            self.ax9.set_ylim(0, 1)  # Set the y-axis limits to 0 and 1
-            self.ax9.set_xlabel('X')
-            self.ax9.set_ylabel('Y')
-            self.ax9.set_title('Normalized Lidar Range Image')
+    def init_plot(self):
+        plt.ion()
+        self.fig9, self.ax9 = plt.subplots()
+        self.ax9.set_xlim(0, 1)  # Set the x-axis limits to 0 and 1
+        self.ax9.set_ylim(0, 1)  # Set the y-axis limits to 0 and 1
+        self.ax9.set_xlabel('X')
+        self.ax9.set_ylabel('Y')
+        self.ax9.set_title('Normalized Observation')
+        self.lidar_plot = None
+        self.goal_plot = None
 
-        self.ax9.clear()
+    def plot_observation(self, lidar_range_image, goal_angle, goal_distance):
+        if self.lidar_plot is not None:
+            self.lidar_plot.remove()
+
+        if self.goal_plot is not None:
+            self.goal_plot.remove()
 
         # Convert lidar observations from polar to Cartesian coordinates
         angles = np.linspace(0, 2 * np.pi, len(lidar_range_image))
@@ -168,19 +176,18 @@ class SparseLidarObservationWrapper(gym.ObservationWrapper):
         y_obs = lidar_range_image * np.sin(angles)
 
         # Plot lidar observations
-        self.ax9.scatter(x_obs, y_obs, c='b', label='Lidar Observations')
+        self.lidar_plot = self.ax9.scatter(x_obs, y_obs, c='b', label='Lidar Observations')
 
         # Convert goal position from polar to Cartesian coordinates
         x_goal = goal_distance * np.cos(goal_angle)
         y_goal = goal_distance * np.sin(goal_angle)
 
         # Plot goal position
-        self.ax9.scatter(x_goal, y_goal, c='r', label='Goal Position')
+        self.goal_plot = self.ax9.scatter(x_goal, y_goal, c='r', label='Goal Position')
 
         self.ax9.legend()
         self.fig9.canvas.draw()
         plt.pause(0.001)
-
 
 
         
