053ac67aa3ef6407b52ce695f5fe0ce896d4ca13
Bram Odrosslij <bram.odrosslij@nobleo.nl>
2024-11-06 11:57:22 +0100

    Step used in reset instead and added init state check

--- environments/base_env.py
+++ environments/base_env.py
@@ -51,45 +51,35 @@ class BaseEnv(gym.Env):
         self.sim_env.reset_map(proto_name)
         self.sim_env.reset_robot(self.init_pose)
         
+        # Precomputations
         self.collision_tree = et.compute_collision_detection_tree(self.map)
         self.map_bounds_polygon = et.compute_map_bound_polygon(self.map, self.cfg.map_padding)
-
-        # # Updating prev_pos, cur_pos, cur_orient, footprint in global frame, and getting new local goal
+       
+        # Resetting the environment state
         self.cur_pos = self.sim_env.robot_position
         self.cur_orient_matrix = self.sim_env.robot_orientation
-        self.footprint_glob = et.get_global_footprint_location(
-            self.cur_pos, 
-            self.cur_orient_matrix, 
-            self.cfg.vehicle.dimensions.polygon_coordinates
-        )
-        self.local_goal_pos = et.get_local_goal_pos(
-            self.cur_pos, 
-            self.cur_orient_matrix, 
-            self.goal_pose
-        )
         self.cur_vel, self.cmd_vel = np.array([0.0, 0.0]), np.array([0.0, 0.0])
+        self.observation, _, done, _, info = self.step(self.cmd_vel)
         
-        self.observation = self.get_obs()
+        # Check if the initial pose is valid, if not reset the environment again
+        if done:
+            self.reset(seed=seed, options=options)
+        
+        # Rendering
         self.render(method='reset')
 
-        return self.observation, {} # = info
+        return self.observation, info
 
     def step(self, action):
         # NOTE: u must use an action wrapper for turning the action vector into a cmd_vel
         self.cmd_vel = action
-
-        # Updating previous values before updating
-        self.prev_pos = self.cur_pos
-        self.prev_observation = self.observation
            
         # Inacting the action (i.e. limit velocity to kinematically feasible values and forward simulate robot)
         self.cur_vel = et.apply_kinematic_constraints(self.sample_time, self.cfg.vehicle.kinematics, self.cur_vel, self.cmd_vel)
         pos, orient = et.compute_new_pose(self.sample_time, self.cur_pos, self.cur_orient_matrix, self.cur_vel)
-        
         self.sim_env.step(pos, orient)
 
-        # Updating prev_pos, cur_pos, cur_orient, footprint in global frame, and getting new local goal
-        self.prev_pos = self.cur_pos
+        # Updating cur_pos, cur_orient, footprint in global frame, and getting new local goal
         self.cur_pos = self.sim_env.robot_position
         self.cur_orient_matrix = self.sim_env.robot_orientation
         self.footprint_glob = et.get_global_footprint_location(
@@ -103,11 +93,12 @@ class BaseEnv(gym.Env):
             self.goal_pose
         )
 
+        # Getting new observation, done, reward
         self.observation = self.get_obs()
-        self.done, self.done_cause = self.get_done()
-        if self.done:
-            print(f"Done cause: {self.done_cause}")            
+        self.done, self.done_cause = self.get_done()        
         self.reward = self.get_reward()
+        
+        # Rendering
         self.render(method='step')
 
         return self.observation, self.reward, self.done, False, {} # last 3: done, truncated, info
