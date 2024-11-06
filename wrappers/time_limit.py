debca75907dd4eff0c31b2f9be1e7086a645a0a1
Bram Odrosslij <bram.odrosslij@nobleo.nl>
2024-11-06 14:19:06 +0100

    Fixed and tested time limit wrapper

--- wrappers/time_limit.py
+++ wrappers/time_limit.py
@@ -1,5 +1,5 @@
 from gymnasium.wrappers import TimeLimit
 
-class TimeLimitWrapper(TimeLimit):
+class TimeLimit(TimeLimit):
     def __init__(self, env, cfg):
         super().__init__(env=env, max_episode_steps=int(cfg.max_episode_steps))
